1b2590d87436b979a9658a813667ae39dcd11a0b
Ardiansyah <eviewnicks@gmail.com>
2025-06-25 21:51:15 +0800

    debug ( TSK-24 )linting error, test, and build

--- jest.setup.js
+++ jest.setup.js
@@ -18,7 +18,7 @@ jest.mock('next/router', () => ({
 jest.mock('next/image', () => ({
   __esModule: true,
   default: (props) => {
-    // eslint-disable-next-line @next/next/no-img-element
+    // eslint-disable-next-line @next/next/no-img-element, jsx-a11y/alt-text
     return <img {...props} />;
   },
 }));
